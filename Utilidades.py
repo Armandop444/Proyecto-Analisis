a4381f0d8694273f20235850969020168a01898e
Alejandro Rivas <alejandrorh97@outlook.com>
2021-04-06 17:36:23 -0600

    Correcion y mejoras con funcion raiz

--- Utilidades.py
+++ Utilidades.py
@@ -1,11 +1,23 @@
 class ParentesisError(Exception):
     pass
 
+class MathError(Exception):
+    pass
+
 def raiz(radicando, indice):
-    if radicando <= 0:
-        return -1 * (abs(radicando) ** (1/indice))
+    if indice < 1:
+        raise MathError("Solo indices enteros mayor que 1")
     else:
-        return radicando ** (1/indice)
+        if indice%2 == 1:
+            if radicando < 0:
+                return -1 * (abs(radicando) ** (1/indice))
+            else:
+                return radicando ** (1/indice)
+        else:
+            if radicando < 0:
+                raise MathError("Radicando negativo")
+            else:
+                return radicando ** (1/indice)
 
 def cifras_significativas(n: int):
     return 0.5*10**(2-n)
