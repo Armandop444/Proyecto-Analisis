201f21e0c88e1c029be18620f39017096f149a25
OC18018 <OC18018@ues.edu.sv>
2021-04-25 20:55:42 -0600

    "Se agrego el nuevo metodo de raiz "

--- FormulaEngine.py
+++ FormulaEngine.py
@@ -1,3 +1,5 @@
+from Utilidades import raiz
+
 def validar_parentesis(funcion: str):
     if funcion.count("(") == funcion.count(")"):
         return True
@@ -8,6 +10,10 @@ def convertir_funcion(formula: str, var_o = "x", var_n = "x"):
     if validar_parentesis(formula):
         #Cambiamos la variable original por la que se va a evaluar
         formula = formula.replace(var_o, var_n)
+
+        #ver si hay raiz y convertila a lenguaje entendible
+        if "raiz" in formula:
+            formula = raiz(formula)
         
         #Convertir funciones trigonometricas
         if "sen" in formula:
@@ -20,7 +26,7 @@ def convertir_funcion(formula: str, var_o = "x", var_n = "x"):
             formula = formula.replace("e^", "exp")
         if "^" in formula:
             formula = formula.replace("^", "**")
-            
+        
         return formula
     else:
         return "Error"
--- Utilidades.py
+++ Utilidades.py
@@ -1,4 +1,5 @@
 from tabulate import tabulate
+import re
 
 
 #Excepciones necesarias para mostrar el tipo de error
@@ -32,7 +33,116 @@ class tablita:
 
 
 # funciones utiles
-def raiz(radicando, indice = 2):
+
+def cifras_significativas(n: int):
+    return 0.5*10**(2-n)
+
+def separarRaiz(valor):
+    valor=valor.lower()
+    parentesisA=0
+    bandera=0
+    salida=""
+    cuantasRaices=valor.count("raiz")
+    inicioBuscar=0
+    if cuantasRaices>0:
+        while cuantasRaices>0:
+            if inicioBuscar==0:
+                posicion=valor.find("raiz")+4
+                posicionInicio=valor.find("raiz")
+            else:
+                posicion=valor.find("raiz",inicioBuscar)+4
+                posicionInicio=valor.find("raiz",inicioBuscar)
+            while bandera == 0:
+
+                if valor[posicion]=="(":
+                    parentesisA=parentesisA+1
+                elif valor[posicion]==")":
+                    parentesisA=parentesisA-1
+
+                if parentesisA==0:
+                    salida=salida+valor[inicioBuscar:posicionInicio]+" "+valor[posicionInicio:(posicion+1)]+" "
+                    bandera=1
+                    inicioBuscar=posicion+1
+        
+                posicion=posicion+1
+            cuantasRaices=cuantasRaices-1
+            bandera=0
+        return salida
+    else:
+        return valor
+
+def unirRaiz(original,convertido=[]):
+    parentesis=0
+    bandera=0
+    salida=""
+    cuantasRaices=original.count("raiz")
+    inicioBuscar=0
+    posicionConvertido=0
+    while cuantasRaices>0:
+        if inicioBuscar==0:
+            posicion=original.find("raiz")+4
+            posicionInicio=original.find("raiz")
+        else:
+            posicion=original.find("raiz",inicioBuscar)+4
+            posicionInicio=original.find("raiz",inicioBuscar)
+        while bandera == 0:
+
+            if original[posicion]=="(":
+                parentesis=parentesis+1
+            elif original[posicion]==")":
+                parentesis=parentesis-1
+
+            if parentesis==0:
+                if inicioBuscar==posicionInicio and original.count("raiz")<=1:
+                    salida=salida+convertido[posicionConvertido]+original[(posicion+1):]
+                else:
+                    salida=salida+original[inicioBuscar:posicionInicio]+convertido[posicionConvertido]
+                bandera=1
+                inicioBuscar=posicion+1
+                posicionConvertido=posicionConvertido+1
+
+            posicion=posicion+1
+        cuantasRaices=cuantasRaices-1
+        bandera=0
+    return salida
+
+def raiz(radicando):
+    separado=separarRaiz(radicando)
+    lista=re.findall(r"raiz(\([\w,\(\^)+*\/-]+\))",separado)
+    cuantosDatos = len(lista)
+    if cuantosDatos>0:    
+        listaConvertida=[]
+        for i in range(cuantosDatos):
+            dato = lista[i]
+            if dato.count(",") == 1:
+                separarIndice = dato.split(",")
+
+                conversion = separarIndice[0]+")"
+                indice = separarIndice[1]
+
+                for k in range(len(indice)):
+                    if indice[k] != ")":
+                        if indice[k].isdigit() == True:
+                            conversion = conversion+"^(1/"+indice[k]+")"
+                        elif (indice[k] == "+" or indice[k] == "-" or indice[k] == "/" or indice[k] == "*") and k==(len(indice)-1):
+                            conversion = conversion+indice[k]
+                        else:
+                            raise MathError(f"Error en funcion raiz({dato}) Solo indices naturales mayor o igual a 1")
+                listaConvertida.append(conversion)
+            elif dato.count(",") >= 1:
+                raise MathError(f"Error en funcion raiz({dato}) Solo indices naturales mayor o igual a 1")
+            else:
+                for k in range(len(dato)):
+                    conversion=dato+"^(1/2)"
+                listaConvertida.append(conversion)
+
+        salida = unirRaiz(radicando,listaConvertida)
+        return salida
+    else:
+        return radicando
+
+#Recuerdame
+def raizA(radicando, indice = 2):
     if not type(indice) == int:
         raise MathError(f"Error en funcion raiz({radicando},{indice}) Solo indices naturales mayor o igual a 1")
     if indice < 1:
@@ -49,5 +159,4 @@ def raiz(radicando, indice = 2):
             else:
                 return radicando ** (1/indice)
             
-def cifras_significativas(n: int):
-    return 0.5*10**(2-n)
+
