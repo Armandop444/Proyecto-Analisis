beceba5f8eed3c6d2a7b6c6269b34aed11e0803b
MR17009 <MR17009@ues.edu.sv>
2021-05-18 15:55:25 -0600

    Mejoramiento de como se muestra el ressultado del ejercicio2 y optimizacion de la formula

--- Unidad1/Ejercicio2.py
+++ Unidad1/Ejercicio2.py
@@ -2,6 +2,7 @@
 # e^x^2
 def ejercicio2():
     import math
+    import numpy as np
     x = float(input("Ingrese el valor de x: "))
     n = int(input("Ingrese la cantidad de cifras significativas: "))
     Es = 0.5 * (math.pow(10,(2-n)))
@@ -11,14 +12,14 @@ def ejercicio2():
     xn0 = 1.0+ math.pow(x,2)
     #print(xn0)
     xn1 = 0.0
-    print("Iteracion|\t Valor Aproximado |\tEa")
-    print(i,"\t|\t ",xn0,"\t|\t - ")
+    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    print("%-20d %-20s %-20s" % (i, xn0, "-"))
     i=2
     while Ea>=Es:
-        xn1 = xn0 +((math.pow(x,(2*(i-1))))/math.factorial(i))
+        xn1 = xn0 +((np.power(x,(2*(i))))/math.factorial(i))
         Ea = float(abs(((xn1 - xn0) / xn1)) * 100)
         xn0=xn1
-        print(i, "\t|\t", xn1, "\t|\t", Ea)
+        print("%-20d %-20s %-20s" % (i, xn1, Ea))
         i+=1
 
 ejercicio2()
