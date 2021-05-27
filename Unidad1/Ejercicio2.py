065462e54aa0e3efde183eb38f35ee2332667e7d
OC18018 <OC18018@ues.edu.sv>
2021-05-26 21:49:55 -0600

    ejercicio 2 arreglado para menu

--- Unidad1/Ejercicio2.py
+++ Unidad1/Ejercicio2.py
@@ -1,25 +1,25 @@
 # Ejercicio2 Unidad1
 # e^x^2
-def ejercicio2():
-    import math
-    import numpy as np
-    x = float(input("Ingrese el valor de x: "))
-    n = int(input("Ingrese la cantidad de cifras significativas: "))
-    Es = 0.5 * (math.pow(10,(2-n)))
+from Utilidades import raiz, tablita
+from sympy import exp, Pow, factorial
+def ejercicio2(x,Es):
+    tabla=tablita(["xn","ea"])
     #print(Es)
     Ea = 100000
     i = 1
-    xn0 = 1.0+ math.pow(x,2)
+    xn0 = 1.0+ Pow(x,2)
+    tabla.add_fila([xn0,"-"])
     #print(xn0)
     xn1 = 0.0
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
-    print("%-20d %-20s %-20s" % (i, xn0, "-"))
+    #print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    #print("%-20d %-20s %-20s" % (i, xn0, "-"))
     i=2
     while Ea>=Es:
-        xn1 = xn0 +((np.power(x,(2*(i))))/math.factorial(i))
+        xn1 = xn0 +((Pow(x,(2*(i))))/factorial(i))
         Ea = float(abs(((xn1 - xn0) / xn1)) * 100)
         xn0=xn1
-        print("%-20d %-20s %-20s" % (i, xn1, Ea))
+        #print("%-20d %-20s %-20s" % (i, xn1, Ea))
+        tabla.add_fila([xn1,Ea])
         i+=1
 
-ejercicio2()
+    return tabla.get_tabla()
