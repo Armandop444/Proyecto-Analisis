0d0649217c668e3f00b2ed5a11491fedc5bcfcad
MR17009 <MR17009@ues.edu.sv>
2021-05-31 19:05:40 -0600

    Agregando proceso de ejercicio 6 y ejercicio 7 de la unidad 1

---
+++ Unidad1/Ejercicio6.py
@@ -0,0 +1,21 @@
+# Ejercicio6 Unidad1
+# sh x
+def ejercicio6():
+    import math
+    x = float(input("Ingrese el valor de x: "))
+    n = int(input("Ingrese la cantidad de cifras significativas: "))
+    Es = 0.5 * (math.pow(10,(2-n)))
+    Ea = 100000
+    xn0 = 0.0
+    xn1 = 0.0
+    i=0
+    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    while Ea>=Es:
+        xn1 = xn0 + ((math.pow(x,(2*i)+1))/math.factorial((2*i)+1))
+        Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
+        xn0=xn1
+        print("%-20d %-20s %-20s" % (i, xn1, Ea))
+        i+=1
+
+
+ejercicio6()
