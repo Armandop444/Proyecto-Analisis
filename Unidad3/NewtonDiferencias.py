9a083ceb23b565689663824d062c08c2b93762c3
Alejandro Rivas <alejandrorrh97@gmail.com>
2021-06-04 17:38:03 -0600

    Tuneado el Newton Diferencias Dividas

--- Unidad3/NewtonDiferencias.py
+++ Unidad3/NewtonDiferencias.py
@@ -1,92 +1,84 @@
-import numpy as np
-import sympy as sym
+from Utilidades import limpiar, tablita
+from sympy import poly_from_expr
 
-# INGRESO , Datos de prueba
-#xi = np.array([4, -4, 7,6,2])
-#fi = np.array([278, -242, 1430, 908,40])
-grado =int(input("Ingrese el grado del polinomio"))
-
-def InterpolacionNewtondDivididas(grado):
-    cont = 0
-    numero = 0
-    final = grado
-    final = int(final)
-    xi = [""]*(final+1)
-    while cont < final+1:
-        mensaje = "Ingrese el valor de Xi para la posicion: {0} \n".format(cont+1)
-        numero = float(input(mensaje))
-        xi[cont] = numero
-        cont = cont+1
-    print(xi)
-    cont = 0
-    fi = [""]*(final+1)
-    while cont < final+1:
-        #print("estoy en segundo bucle con el contador en {0}".format(cont))
-        mensaje = "Ingrese el valor de fi para la posicion: {0} \n".format(cont+1)
-        numero = float(input(mensaje))
-        fi[cont] = numero
-        cont = cont+1
-    print(fi)
-
-
-    titulo = ['i   ', 'xi  ', 'fi  ']
-    n = len(xi)
-    ki = np.arange(0, n, 1)
-    tabla = np.concatenate(([ki], [xi], [fi]), axis=0)
-    tabla = np.transpose(tabla)
-
-
-    dfinita = np.zeros(shape=(n, n), dtype=float)
-    tabla = np.concatenate((tabla, dfinita), axis=1)
-
-
-    [n, m] = np.shape(tabla)
-    diagonal = n-1
-    j = 3
-    while (j < m):
-
-        titulo.append('F['+str(j-2)+']')
-
-        i = 0
-        paso = j-2
-        while (i < diagonal):
-            denominador = (xi[i+paso]-xi[i])
-            numerador = tabla[i+1, j-1]-tabla[i, j-1]
-            tabla[i, j] = numerador/denominador
-            i = i+1
-        diagonal = diagonal - 1
-        j = j+1
-
-
-    dDividida = tabla[0, 3:]
-    n = len(dfinita)
-
-
-    x = sym.Symbol('x')
-    polinomio = fi[0]
-    for j in range(1, n, 1):
-        factor = dDividida[j-1]
-        termino = 1
-        for k in range(0, j, 1):
-            termino = termino*(x-xi[k])
-        polinomio = polinomio + termino*factor
-
-
-    polisimple = polinomio.expand()
-
-
-    # SALIDA
-
-    print('dDividida: ')
-    print(dDividida)
-    print('polinomio: ')
-    print(polinomio)
-    print('polinomio simplificado: ')
-    print(polisimple)
-    punto = float(input("Ingrese el punto a evaluar \n"))
-    evaluar = polisimple.subs(x, punto)
-    print(evaluar)
-    print(tabla)
-
-#Prueba de funcion aquie agregar el grado del polinomio
-InterpolacionNewtondDivididas(grado)
+def pedir_valores():
+    total = 0
+    xi = []
+    while True:
+        limpiar()
+        print("Presione 'S' para pasar a ingresar los valores de fi")
+        print(f"\tXi ingresados: {xi}")
+        dato = input(f"\tIngrese el valor de x{len(xi)}: ")
+        if dato.upper() == "S":
+            break
+        else:
+            try:
+                xi.append(str(float(dato)))
+            except:
+                continue
+    fi = []
+    while True:
+        limpiar()
+        print("Presione 'S' para terminar")
+        print(f"\tXi ingresados: {xi}")
+        print(f"\tXi ingresados: {fi}")
+        dato = input(f"\tIngrese el valor de f{len(fi)}: ")
+        if dato.upper() == "S":
+            break
+        else:
+            try:
+                fi.append(float(dato))
+            except:
+                continue
+    valores = {}
+    for i in range(len(xi)):
+        valores[str(xi[i])] = [fi[i]]
+    valores['total'] = len(xi)
+    return valores
+    
+def NewtonDD():
+    datos = pedir_valores()
+    #crear tabla
+    cabezero = ["Zk","f(Zk)"]
+    for _ in range(datos["total"] -1):
+        cabezero.append("")
+    tabla = tablita(cabezero, show_iteracion=False)
+    
+    #cargar matriz principal
+    matriz = []
+    for xn, fx in datos.items():
+        if not xn == "total":
+            for i in range(len(fx)):
+                fila = [float(xn)]
+                fila.append(fx[0])
+                for _ in range(datos["total"]-1):
+                    fila.append(float())
+                matriz.append(fila)
+    
+    #calcular
+    #j,i
+    for i in range(2,datos["total"]+1):
+        for j in range(i-1,len(matriz)):
+            #print(f"({j},{i})")
+            if not (matriz[j][0] - matriz[j-(i-1)][0]) == 0:
+                matriz[j][i] = (matriz[j][i-1] - matriz[j-1][i-1]) / (matriz[j][0] - matriz[j-(i-1)][0])
+            else:
+                matriz[j][i] = datos[str(matriz[j][0])][i-1]
+    
+    polinomio = f"{matriz[0][1]}"
+    temp = ""
+    for j in range(1,len(matriz)):
+        #print(f"{matriz[j][0]} {matriz[j][j+1]}")
+        temp += f"*(x-{matriz[j-1][0]})"
+        polinomio += f"+({matriz[j][j+1]}){temp}"
+        
+    x,_ = poly_from_expr(polinomio)
+    
+    for fila in matriz:
+        tabla.add_fila(fila)
+        
+    print(f"{'*'*5} Resultado del polinomio de Hermite {'*'*5} \n")
+    tabla.print_table()
+    print(f"\nPolinomio: {str(x.expr).replace('**','^')}")
+    print("\n","*"*50)
+    return str(x.expr) 
