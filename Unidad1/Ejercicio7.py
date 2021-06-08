02595bc66a8cb187bfa62de25c574e6dc6f84d45
OC18018 <OC18018@ues.edu.sv>
2021-06-08 13:04:42 -0600

    ejercicio 7 editado para menu

--- Unidad1/Ejercicio7.py
+++ Unidad1/Ejercicio7.py
@@ -1,21 +1,21 @@
 # Ejercicio7 Unidad1
-# sh x
-def ejercicio7():
-    import math
-    x = float(input("Ingrese el valor de x: "))
-    n = int(input("Ingrese la cantidad de cifras significativas: "))
-    Es = 0.5 * (math.pow(10,(2-n)))
+# ch x
+from Utilidades import raiz, tablita
+from sympy import ln, exp, Pow, factorial
+def ejercicio7(x,Es):
+    tabla=tablita(["xn1","Ea"])
     Ea = 100000
-    xn0 = 0.0
+    xn0 = 1.0
     xn1 = 0.0
     i=0
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    #print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    tabla.add_fila([xn0,"-"])
     while Ea>=Es:
-        xn1 = xn0 + ((math.pow(x,2*i))/math.factorial(2*i))
-        Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
-        xn0=xn1
-        print("%-20d %-20s %-20s" % (i, xn1, Ea))
+        if i!=0:
+            xn1 = xn0 + ((Pow(x,2*i))/factorial(2*i))
+            Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
+            xn0=xn1
+            #print("%-20d %-20s %-20s" % (i, xn1, Ea))
+            tabla.add_fila([xn1,Ea])
         i+=1
-
-
-ejercicio7()
+    return tabla.get_tabla()
