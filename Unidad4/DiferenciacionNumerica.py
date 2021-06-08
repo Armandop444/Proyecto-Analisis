461c0727d2fe5bb4766d54af737ab3d27aa73eb9
AionAdrammelech <pjosesand699@gmail.com>
2021-06-08 14:30:57 -0600

    hacia atras

--- Unidad4/DiferenciacionNumerica.py
+++ Unidad4/DiferenciacionNumerica.py
@@ -15,9 +15,38 @@ def diffforwardfunc(funcion, xo, h, nivelderivada):
         print("error en la seleccion de nivel de derivada")
     return float(respuesta)
 
-funcion="-(0.10*(x**4))-(0.15*(x**3))-(0.5*(x**2))-(0.25*x)+1.2"
-xo=0.5
-h=0.1
-uwu=diffforwardfunc(funcion,xo,h,1)
+def diffbackwardfunc(funcion,xo,h,nivelderivada):
+    respuesta=0
+    error=0.0
+    funcion=parse_expr(funcion)
+    funcdiff=parse_expr("1")
+    if nivelderivada==1:
+        funcdiff=(-(funcion.subs(x,(xo-h)))+(funcion.subs(x,(xo))))/(h)
+        respuesta=funcdiff
+    elif nivelderivada==2:
+        funcdiff=((funcion.subs(x,(xo-2*h)))-4*(funcion.subs(x,(xo-h)))+3*(funcion.subs(x,xo)))/(2*h)
+        respuesta=funcdiff
+    else:
+        print("error en la seleccion de nivel de derivada")
+    return float(respuesta)
+
+
+#opcionmetodo
+#1=centradasegundoorden
+#2=centradacuartoorden
+#3=3puntos
+#4=5puntos
+def diffcentralfunc(funcion,xo,h,opcionmetodo):
+    #codigo
+    respuesta=0
+    return float(respuesta)
+
+
+
+
+funcion="log(x)"
+xo=5
+h=0.05
+uwu=diffbackwardfunc(funcion,xo,h,2)
 print(uwu)
 
