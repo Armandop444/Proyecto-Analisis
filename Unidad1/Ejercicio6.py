8082c1cdd28676b38aed857042e2b68ee18920ad
OC18018 <OC18018@ues.edu.sv>
2021-06-08 12:58:55 -0600

    ejercicio6 editado para menu

--- Unidad1/Ejercicio6.py
+++ Unidad1/Ejercicio6.py
@@ -1,21 +1,18 @@
 # Ejercicio6 Unidad1
-# ch x
-def ejercicio6():
-    import math
-    x = float(input("Ingrese el valor de x: "))
-    n = int(input("Ingrese la cantidad de cifras significativas: "))
-    Es = 0.5 * (math.pow(10,(2-n)))
+# sh x
+from Utilidades import raiz, tablita
+from sympy import ln, exp, Pow, factorial
+def ejercicio6(x,Es):
+    tabla=tablita(["Xn1","Ea"])
     Ea = 100000
     xn0 = 0.0
     xn1 = 0.0
     i=0
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
     while Ea>=Es:
-        xn1 = xn0 + ((math.pow(x,(2*i)+1))/math.factorial((2*i)+1))
+        xn1 = xn0 + ((Pow(x,(2*i)+1))/factorial((2*i)+1))
         Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
         xn0=xn1
-        print("%-20d %-20s %-20s" % (i, xn1, Ea))
+        #print("%-20d %-20s %-20s" % (i, xn1, Ea))
+        tabla.add_fila([xn1,Ea])
         i+=1
-
-
-ejercicio6()
+    return tabla.get_tabla()
