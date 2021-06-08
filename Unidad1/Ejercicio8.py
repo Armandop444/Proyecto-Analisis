d4cc048e2586df39d190f0fd19968d5f5cb13250
OC18018 <OC18018@ues.edu.sv>
2021-06-08 13:35:44 -0600

    Ejercicio 8 y 9 editado para menu

--- Unidad1/Ejercicio8.py
+++ Unidad1/Ejercicio8.py
@@ -1,26 +1,21 @@
 # Ejercicio8 Unidad1
 # arcsen x
-def ejercicio8():
-    import math
-    import numpy as np
-    x = float(input("Ingrese el valor de x: "))
-    n = int(input("Ingrese la cantidad de cifras significativas: "))
-    Es = 0.5 * (math.pow(10,(2-n)))
+from Utilidades import tablita
+from sympy import Pow, factorial
+def ejercicio8(x,Es):
     Ea = 100000
     xn0 = 0.0
     xn1 = 0.0
     i=0
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    tabla=tablita(["xn1","Ea"])
     while Ea>=Es:
         if x!=0:
-            xn1 = xn0 + (((math.factorial(2*i))/(math.pow(((math.pow(2,i))*math.factorial(i)),2)))*((math.pow(x,(2*i)+1))/(((2*i)+1))))
+            xn1 = xn0 + (((factorial(2*i))/(Pow(((Pow(2,i))*factorial(i)),2)))*((Pow(x,(2*i)+1))/(((2*i)+1))))
             Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
             xn0=xn1
-            print("%-20d %-20s %-20s" % (i, xn1, Ea))
+            tabla.add_fila([xn1,Ea])
         else:
-            print("%-20d %-20s %-20s" % (1, 0, 0))
+            tabla.add_fila([0,0])
             break
         i+=1
-
-
-ejercicio8()
+    return tabla.get_tabla()
--- Unidad1/Ejercicio9.py
+++ Unidad1/Ejercicio9.py
@@ -1,25 +1,23 @@
 # Ejercicio9 Unidad1
 # ln(1+x)
-def ejercicio9():
-    import math
-    x = float(input("Ingrese el valor de x: "))
-    n = int(input("Ingrese la cantidad de cifras significativas: "))
-    Es = 0.5 * (math.pow(10,(2-n)))
+from Utilidades import tablita
+from sympy import Pow, factorial
+def ejercicio9(x,Es):
     Ea = 100000
     xn0 = 0.0
     xn1 = 0.0
     i=1
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    #print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
+    tabla=tablita(["xn1","Ea"])
     while Ea>=Es:
         if x!=0:
-            xn1 = xn0 + ((math.pow(-1,i-1))*((math.pow(x,i))/(i)))
+            xn1 = xn0 + ((Pow(-1,i-1))*((Pow(x,i))/(i)))
             Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
             xn0=xn1
-            print("%-20d %-20s %-20s" % (i, xn1, Ea))
+            #print("%-20d %-20s %-20s" % (i, xn1, Ea))
+            tabla.add_fila([xn1,Ea])
         else:
-            print("%-20d %-20s %-20s" % (i, 0, 0))
+            #print("%-20d %-20s %-20s" % (i, 0, 0))
             break
         i+=1
-
-
-ejercicio9()
+    return tabla.get_tabla()
