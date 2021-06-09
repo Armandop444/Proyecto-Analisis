e69639872a3b5333cf6b9a20913de06b765c850c
VM18042 <vm18042@ues.edu.sv>
2021-06-09 17:20:13 -0600

    Agregado calcular error richardson

--- Unidad4/Richardson.py
+++ Unidad4/Richardson.py
@@ -1,7 +1,7 @@
 #from Utilidades import limpiar, tablita
 from sympy import *
 import numpy as np
-#from FormulaEngine import convertir_funcion, reconvertir_funcion
+from FormulaEngine import convertir_funcion, reconvertir_funcion
 from sympy import cos, sin, tan, log, ln, exp, cot, sec, csc, asin, acos, atan, Symbol,factorial, diff, parse_expr, Abs
 from DiferenciacionNumerica import *
 
@@ -36,11 +36,17 @@ def nivel4(D1,D2):
     respuesta=((64/63)*D2)-((1/63)*D1)
     return respuesta
 
+def CalcularError(func,x,Matriz,nivel):
+    #(f(x)-f(xn))/f(x)
+    error=float(((eval(convertir_funcion(str(func),var_n="x")))-(Matriz[nivel-1,nivel]))/(eval(convertir_funcion(str(func),var_n="x"))))
+    return error
+
 def Richardson():
     func=input("Ingrese la funcion a evaluar\n")
     x=float(input("Ingrese el punto a evaluar en la funcion\n"))
     h=input("Ingrese el valor de h\n")
-    nivel=int(input("Ingrese el nivel de la diferenciacion\n"))
+    nivel=int(input("Ingrese el nivel de la diferenciacion (Entre 2 y 4)\n"))
+    #↓↓↓↓↓creando matriz↓↓↓↓↓
     Matriz = np.zeros((nivel, nivel+1))
     Matriz=llenarH(h,Matriz,nivel)
     print("Eliga el metodo de Diferencias Divididas: \n1.Hacia Adelante\n2.Hacia Atras\n3.Centrado")
@@ -63,7 +69,10 @@ def Richardson():
         #cuarto nivel
         for i in range(3,nivel,1):
             Matriz[i,4]=nivel3(Matriz[i-1,3],Matriz[i,3])
+        print("La matriz Resultante es:\n")
         print(Matriz)
+        print("El error calculado es:")
+        print(CalcularError(func,x,Matriz,nivel))
         
     elif metodo==2:#------------------------------------------------------------------------------------
         print("Eliga el orden de la diferencia hacia atras: \n1.Primer orden \n2.Segundo orden")
@@ -82,6 +91,8 @@ def Richardson():
         for i in range(3,nivel,1):
             Matriz[i,4]=nivel3(Matriz[i-1,3],Matriz[i,3])
         print(Matriz)
+        print("El error calculado es:")
+        print(CalcularError(func,x,Matriz,nivel))
         
     elif metodo==3:#------------------------------------------------------------------------------------
         print("Eliga el orden de la diferencia centrada: \n1.Segundo orden\n2.cuarto orden\n3.De 3 puntos\n4.De 5 puntos")
@@ -100,10 +111,11 @@ def Richardson():
         for i in range(3,nivel,1):
             Matriz[i,4]=nivel3(Matriz[i-1,3],Matriz[i,3])
         print(Matriz)
+        print("El error calculado es:")
+        print(CalcularError(func,x,Matriz,nivel))
         
     else:#------------------------------------------------------------------------------------
         print("Error en la eleccion del metodo")
-    return
 
 Richardson()
 
