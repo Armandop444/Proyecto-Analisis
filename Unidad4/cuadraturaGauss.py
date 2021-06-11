5c01b149ecbc2e69e8eed2a5377eef14302adadc
OM17028 <OM17028@ues.edu.sv>
2021-06-11 14:40:19 -0600

    commit integracion
    falta revisar gauss

---
+++ Unidad4/cuadraturaGauss.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sympy import *
+# cuadratura de Gauss de dos puntos
+def integraCuadGauss2p(fx,a,b):
+    x0 = -1/np.sqrt(3)
+    x1 = -x0
+    xa = (b+a)/2 + (b-a)/2*(x0)
+    xb = (b+a)/2 + (b-a)/2*(x1)
+    area = ((b-a)/2)*(fx.subs(x,xa) + fx.subs(x,xb))
+    return(area)
+
+# INGRESO
+ecuacion = input("ingrese la funcion\n")
+
+x = symbols('x')  # declaramos que x es un simbolo
+
+# esto es del ccodigo de alejandro
+if "^" in ecuacion:
+    ecuacion = ecuacion.replace("^", "**")
+# aqui termina el codigo del ale
+
+fx = parse_expr(ecuacion)  # funcion que evaluaremos
+#fx = lambda x: (np.exp(x)*np.sin(x))/(1+x**2)
+
+# intervalo de integración
+a = float(input("ingrese el punto a de la integral\n"))
+b = float(input("ingrese el punto b de la integral\n"))
+tramos = int(input("cuantos puntos desea evaluar\n"))
+
+
+# PROCEDIMIENTO
+muestras = tramos+1
+xi = np.linspace(a,b,muestras)
+area = 0
+for i in range(0,muestras-1,1):
+    deltaA = integraCuadGauss2p(fx,xi[i],xi[i+1])
+    area = area + deltaA
+# SALIDA
+print('Integral: ', area)
