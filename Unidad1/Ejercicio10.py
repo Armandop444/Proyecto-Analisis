a372513399e1044e87e8918bfd35dc6f3355c273
OC18018 <OC18018@ues.edu.sv>
2021-06-15 20:10:56 -0600

    arreglos en ejercicio 10 y 11 para menu

--- Unidad1/Ejercicio10.py
+++ Unidad1/Ejercicio10.py
@@ -3,24 +3,21 @@
 #-1<x<1
 from Utilidades import tablita
 from sympy import Pow, factorial
-def ejercicio10(x=-0.9,Es=0.05):
+def ejercicio10(x,Es):
     Ea = 100000
     xn0 = 0.0
     xn1 = 0.0
     i=0
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
-    #tabla=tablita(["xn1","Ea"])
+    tabla=tablita(["xn1","Ea"])
     while Ea>=Es:
         if x!=0:
             xn1 = xn0 + ((Pow(-1,i))*((Pow(x,2*i))))
             Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
             xn0=xn1
-            print("%-20d %-20s %-20s" % (i, xn1, Ea))
-            #tabla.add_fila([xn1,Ea])
+            tabla.add_fila([xn1,Ea])
         else:
-            print("%-20d %-20s %-20s" % (i, 0, 0))
+            
+            tabla.add_fila([0,0])
             break
         i+=1
-    #return tabla.get_tabla()
-
-ejercicio10()
+    return tabla.get_tabla()
--- Unidad1/Ejercicio11.py
+++ Unidad1/Ejercicio11.py
@@ -1,5 +1,5 @@
 # Ejercicio9 Unidad1
-# 1/1+x^2
+# arctan(x)
 #-1<x<1
 from Utilidades import tablita
 from sympy import Pow, factorial
@@ -8,19 +8,15 @@ def ejercicio11(x,Es):
     xn0 = 0.0
     xn1 = 0.0
     i=0
-    print("%-20s %-20s %-20s" % ("iteracion", "Xn1", "Ea"))
-    #tabla=tablita(["xn1","Ea"])
+    tabla=tablita(["xn1","Ea"])
     while Ea>=Es:
         if x!=0:
             xn1 = xn0 + (((Pow(-1,i))*((Pow(x,(2*i)+1))))/((2*i)+1))
             Ea = float(abs(((xn1 - xn0) /( xn1))) * 100)
             xn0=xn1
-            print("%-20d %-20s %-20s" % (i, xn1, Ea))
-            #tabla.add_fila([xn1,Ea])
+            tabla.add_fila([xn1,Ea])
         else:
-            print("%-20d %-20s %-20s" % (i, 0, 0))
+            tabla.add_fila([0,0])
             break
         i+=1
-    #return tabla.get_tabla()
-
-ejercicio11()
+    return tabla.get_tabla()
