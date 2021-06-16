413af79fa527f69bf8b06f29ed99cef5209f5c19
OC18018 <OC18018@ues.edu.sv>
2021-06-16 13:11:44 -0600

    kutta listo pa menu

--- Unidad5/Kuttaorden4.py
+++ Unidad5/Kuttaorden4.py
@@ -1,5 +1,7 @@
-import numpy as np
-
+from numpy import zeros
+from sympy import cos, sin, tan, log, ln, exp, cot, sec, csc, asin, acos, atan
+from Utilidades import tablita, limpiar
+from FormulaEngine import convertir_funcion
 def f(xi, yi, funcion):
     valor = 0
     if "x" in funcion:
@@ -9,20 +11,35 @@ def f(xi, yi, funcion):
     valor = eval(funcion)
     return valor
 
-def KuttaGrado4(dy, x0, y0, h):
+def imprimir(Matriz,nivel):
+    tabla=tablita(["Xi","Yi"],show_iteracion=False)
+    for xn in range(nivel):
+        columna=[]
+        for fx in range(2):
+            columna.append(Matriz[xn,fx])
+        tabla.add_fila(columna)
+    tabla.print_table()
+
+
+def KuttaGrado4():
+    dy = input("Ingrese la y´")
+    dy=convertir_funcion(dy)
+    x0 = float(input("ingrese X0\n"))
+    y0 = float(input("ingrese Y0\n"))
+    h = float(input("ingrese h\n"))
     i = x0
     pregunta = input(
         "Como quiere operar\n1.Con # de iteraciones\n2.Evaluacion en un punto de f(x)")
     if pregunta == "2":
         xF = float(input("Ingrese el punto de f(x)"))
         muestras = 0
-        while i < xF:
+        while i <= xF:
             muestras = muestras+1
             i += h
     else:
         muestras = int(input("Ingrese el # de iteraciones que desea hacer"))
     tamano = muestras + 1
-    estimado = np.zeros(shape=(tamano, 2), dtype=float)
+    estimado = zeros(shape=(tamano, 2), dtype=float)
 
     # incluye el punto [x0,y0]
     estimado[0] = [x0, y0]
@@ -38,19 +55,4 @@ def KuttaGrado4(dy, x0, y0, h):
         xi = xi + h
 
         estimado[i] = [xi, yi]
-    return (estimado)
-
-
-dy = input("Ingrese la y´")
-x0 = float(input("ingrese X0\n"))
-y0 = float(input("ingrese Y0\n"))
-h = float(input("ingrese h\n"))
-
-# PROCEDIMIENTO
-puntosRK2 = KuttaGrado4(dy, x0, y0, h)
-xi = puntosRK2[:, 0]
-yiRK2 = puntosRK2[:, 1]
-
-# SALIDA
-print('estimado[xi,yi]')
-print(puntosRK2)
+    imprimir(estimado, tamano-1)
