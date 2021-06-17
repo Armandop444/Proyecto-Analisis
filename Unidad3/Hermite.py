648faa50222df48a1058f47b1fd9971731072d00
AionAdrammelech <pjosesand699@gmail.com>
2021-06-17 14:33:47 -0600

    punto vacio opcional

--- Unidad3/Hermite.py
+++ Unidad3/Hermite.py
@@ -28,7 +28,7 @@ def hermiteNormal():
     fxn=pedir_valores("fi",len((xn)))
     dfxn=pedir_valores("y'",len(fxn))
     limpiar()
-    punto=float(input("Ingrese el punto a evaluar: "))
+    punto=input("Ingrese el punto a evaluar: ")
     ls=[]
     lsdiff=[]
     h=[]
@@ -77,5 +77,6 @@ def hermiteNormal():
     print(polhermite)
     print("")
     if punto!="":
+        punto=float(punto)
         print("el valor del punto dado ({0}) en el polinomio de hermite es: ".format(punto))
         print((polhermite.subs(x,punto)).expand())
