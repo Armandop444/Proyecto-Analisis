b6c0fe2967956a9a57229ac5f06c9a833a2cc3c2
VM18042 <vm18042@ues.edu.sv>
2021-06-17 16:15:56 -0600

    agregada convergencia NewRhap

--- Unidad2/newtonRaphson.py
+++ Unidad2/newtonRaphson.py
@@ -1,23 +1,27 @@
 from FormulaEngine import convertir_funcion,raiz
 from Utilidades import tablita
-from sympy import cos, sin, tan, log, ln, exp, cot, sec, csc, asin, acos, atan, diff, symbols
+from sympy import cos, sin, tan, log, ln, exp, cot, sec, csc, asin, acos, atan, diff, symbols, parse_expr
 
 def NewtonRaphson(funcion:str, xo: float, es: float):
+    x = symbols('xo')
     tabla = tablita(['Xn','f(Xn)',"f'(Xn)", "Xn+1","EA"])
     ea = 100
     
     funcion = convertir_funcion(funcion,var_n='xo')
-    
     #Calcular derivadas
-    x = symbols('xo')
-    df = diff(funcion,x)
-    
-    while ea > es:
-        fx = float(eval(funcion))
-        dfx = float(eval(str(df)))
-        
-        xn = xo - (fx/dfx)
-        ea = abs((xn-xo)/xn) * 100
-        tabla.add_fila([xo,fx,dfx,xn,ea])
-        xo = xn
-    return tabla.get_tabla()
+    fx=float(eval(funcion))
+    df = diff(funcion,x)#Haciendo la primer derivada
+    df2= diff(df,x)#haciendo la segunda derivada
+    convergencia=abs((fx*float(eval(str(df2))))/(float(eval(str(df))))**2)#Encontrando el valor de la convergencia
+    if(convergencia<1):#convergencia
+        while ea > es:
+            fx = float(eval(funcion))
+            dfx = float(eval(str(df)))
+            
+            xn = xo - (fx/dfx)
+            ea = abs((xn-xo)/xn) * 100
+            tabla.add_fila([xo,fx,dfx,xn,ea])
+            xo = xn
+        return tabla.get_tabla()
+    else:
+        return "No existe raiz"
