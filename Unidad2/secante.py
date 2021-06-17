40ccb1861bdb80123cc1b76b35f79353f648c911
VM18042 <vm18042@ues.edu.sv>
2021-06-17 16:37:16 -0600

    Eliminando comentarios

--- Unidad2/secante.py
+++ Unidad2/secante.py
@@ -2,29 +2,11 @@ from FormulaEngine import convertir_funcion, raiz
 from Utilidades import tablita
 from sympy import cos, sin, tan, log, ln, exp, cot, sec, csc, asin, acos, atan
 
-
-#ecuacion=input("ingrese la funcion\n")
-# funcion que se evaluara
-
-#ecuacion=input("ingrese la funcion\n")
-#funcion que se evaluara
-
-#esto es del ccodigo de alejandro
-#if"^"in ecuacion:
-#    ecuacion=ecuacion.replace("^","**")
-#aqui termina el codigo del ale
-
-#func = parse_expr(ecuacion)# funcion que evaluaremos
-
-
 def Sec(func, xnmenos, xn, es):
     tabla = tablita(["Xn-1","Xn","Xn+1","EA"])
     itera=1
     xnmas = 0
     ea=100.00
-
-    #f1 = func.subs(x,xnmenos)  # reamplazmos x por x1xnmenos y evaluamos la funcion
-    # incio del bucle
     
     funcion1=eval(convertir_funcion(func,var_n="xnmenos"))
     funcion2=eval(convertir_funcion(func,var_n="xn"))
@@ -47,14 +29,7 @@ def Sec(func, xnmenos, xn, es):
             #si se cumple lo anterior se da el cambio de valores
             xnmenos=float(xn)
             xn=float(xnmas)
-            
-        #print(f"\nEl valor de la raiz es: {xnmas}")
-        #print(f"Con un error de : {ea}\n")
-
         return tabla.get_tabla()
         
     else:
         print("No existe raiz")
-
-#a=Sec(func,0,1, 0.00001)
-#print(a)
