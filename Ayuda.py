423f659833d006262320df2f3c84db91abaf2009
OC18018 <OC18018@ues.edu.sv>
2021-06-17 18:12:39 -0600

    Creado ayuda

---
+++ Ayuda.py
@@ -0,0 +1,20 @@
+
+def ayuda():
+    print("\nSintaxis: (Tener en cuenta la escritura de parentesis)\n"
+    "Simbolos: para elevacion puede ocupar el simbolo circunflejo ^ en caso de no tener el simbolo puede ocupar **\n"
+    "Ejemplo1 x elevado al cuadrado: x^(2) o x**(2)\n"
+    "Ejemplo2 x elevado a la dos tercios: x^(2/3) o x**(2/3)\n\n"
+    "Para funciones trigonometricas ocupar: cos(), sen() o sin(), tan()\n"
+    "Para funciones trigonometricas inversar ocupar: acos,asen() o asin(), atan()\n"
+    "Ejemplo1 para decir arcoseno de x: acos(x)\n"
+    "Ejemplo2 tangente de 1: tan(1)\n\n"
+    "Para logaritmos ocupar: log(funcion,base), ln(funcion)\n"
+    "Ejemplo1 logaritmo de x base 3: log(x,3)\n"
+    "Ejmplo2 logaritmo natural de x: log(x) o ln(x)\n\n"
+    "Para euler o exponencial ocupar : e^(funcion o numero)\n"
+    "Ejemplo1 e elevado a x al cuadrado: e^(x^(2))\n"
+    "Ejemplo2 2 por e: 2*e\n\n"
+    "Para ocupar raices ocupar: raiz(funcion,radicando) o funcion^(1/radicando)\n"
+    "Ejemplo1 raiz cubica de x : raiz(x,3) o x^(1/3)\n"
+    "Ejemplo2 raiz cuadrada de x+2: raiz(x+2) o (x+2)^(1/2)\n\n")
+ayuda()
